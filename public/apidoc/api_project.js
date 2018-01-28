c9608eb4e559a11ffdc391751e5ef0cfe0142dba
Jason Suttles <Jason Suttles>
2018-01-28 14:45:31 -0500

    move apidoc

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-01-28T19:36:21.329Z",
+    "time": "2018-01-28T19:45:06.239Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
